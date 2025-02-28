1230a562f097070941f6127410967e38776430e3
awatersny <awatersny@gmail.com>
2025-02-28 17:42:55 -0500

    Finish solution to part 3.  Keeping first attempt.

--- index.js
+++ index.js
@@ -63,32 +63,68 @@
 
 // Your task is to write a script that accomplishes the following:
 const data = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232"
-// Store each “cell” of data in a variable.
-let formatted = ""
-  // Loop through the characters of a given CSV string.
-  pos = 0
-  while (pos < data.length) {
+// // Store each “cell” of data in a variable.
+// let formatted = ""
+//   // Loop through the characters of a given CSV string.
+//   pos = 0
+//   while (pos < data.length) {
+//     // When you encounter a comma, move to the next cell.
+//     if (data[pos] === ",") {
+//       formatted += "\t\t|"
+//     }
+//     // When you encounter the “\r\n” sequence, move to the next “row.”
+//     else if (data[pos] === "\n" || data[pos] === "\r\n") {
+//       formatted += "\n_______________________________________________________________\n"
+//     } else {
+//       formatted += data[pos]
+//     }
+//     // Log each row of data.
+//     //   You do not need to format the data, the following works well.
+//     //   console.log(cell1, cell2, cell3, cell4);
+//     pos++
+//   }
+// console.log(formatted)
+
+let cell1 = ""
+let cell2 = ""
+let cell3 = ""
+let cell4 = ""
+let commaCount = 0
+// Loop through the characters of a given CSV string.
+for (pos = 0; pos < data.length; pos++) {
+  // Store each “cell” of data in a variable.
+  if (data[pos] !== ",") {
+    if(commaCount === 0) {
+      cell1 += data[pos]
+    }
+    if(commaCount === 1) {
+      cell2 += data[pos]
+    }
+    if(commaCount === 2) {
+      cell3 += data[pos]
+    }
+    if(commaCount === 3) {
+      cell4 += data[pos]
+    }
+  } else {
     // When you encounter a comma, move to the next cell.
-    if (data[pos] === ",") {
-      formatted += "\t\t|"
-    }
-    // When you encounter the “\r\n” sequence, move to the next “row.”
-    else if (data[pos] === "\n" || data[pos] === "\r\n") {
-      formatted += "\n_______________________________________________________________\n"
-    } else {
-      formatted += data[pos]
-    }
-    // Log each row of data.
-    //   You do not need to format the data, the following works well.
-    //   console.log(cell1, cell2, cell3, cell4);
-    pos++
+    commaCount++
   }
-console.log(formatted)
+  // When you encounter the “\r\n” sequence, move to the next “row.”
+  if (data[pos] === "\n" || pos === data.length - 1) {
+    // Log each row of data.
+    // You do not need to format the data, the following works well.
+    console.log(cell1, cell2, cell3, cell4)
+    commaCount = 0
+    cell1 = ""
+    cell2 = ""
+    cell3 = ""
+    cell4 = ""
+  }
+}
 // You can make the following assumptions:
   // There will only be 4 cells per row.
   // There will be no escaped characters other than “\n”.
 
 // Use the example string provided above to test your program. Once you are confident it is working correctly, try the following string to see if your program works properly with other data.
 // Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232
-
-// console.log("Carriage\r\nExample")
